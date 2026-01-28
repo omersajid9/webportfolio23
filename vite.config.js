e913f083860a3f80683100262a2cdcf8cabd4a25
Omer Sajid <omersajid@Mac.fios-router.home>
2026-01-28 01:14:54 -0600

    optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024,
+    })
+  ],
   server: {
     host: '0.0.0.0'
   },
@@ -14,12 +22,18 @@ export default defineConfig({
   build: {
     minify: 'esbuild',
     cssMinify: true,
+    cssCodeSplit: true,
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom']
-        }
+        },
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
-    }
+    },
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1000
   }
 })
